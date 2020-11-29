49d5116a055de5bc9870ec0fc79d6062fdb84b4c
xumu <932851779@qq.com>
2020-11-29 17:25:17 +0800

    1. set eureka configuration 2. register payment8001 3. create commons api jar

--- cloud-eureka-server-7001/src/main/java/com/atguigu/springcloud/EurekaMain7001.java
+++ cloud-eureka-server-7001/src/main/java/com/atguigu/springcloud/EurekaMain7001.java
@@ -2,18 +2,18 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * Description:
- * date: 2020-11-28
+ * date: 2020-11-29
  *
  * @author xumu
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class PaymentMain8001 {
+@EnableEurekaServer
+public class EurekaMain7001 {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class,args);
+        SpringApplication.run(EurekaMain7001.class,args);
     }
 }
