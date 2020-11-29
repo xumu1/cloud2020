310328f3f23923ef4e1660a458f510269959d26b
xumu <932851779@qq.com>
2020-11-29 17:33:10 +0800

    close devtools/hot compile

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
@@ -2,6 +2,7 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Description:
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  * @author xumu
  */
+@EnableEurekaClient
 @SpringBootApplication
 public class OrderMain80 {
     public static void main(String[] args) {
